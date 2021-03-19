03cc945ec12150ee770814b3dc1ea6edb22fb7a0
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-03-18 21:11:48 -0700

    adding chrome driver to repo

--- scrape_mars.py
+++ scrape_mars.py
@@ -5,7 +5,7 @@ import time
 
 
 executable_path = {'executable_path': 'chromedriver.exe'}
-browser = Browser('chrome', **executable_path, headless=True)
+browser = Browser('chrome', **executable_path, headless=False)
 
 
 # create empty dictinary
@@ -13,9 +13,6 @@ mars_info = {}
 
 # NASA Mars News
 def scrape_news():
-
-    # Initialize browser
-    browser = init_browser()
 
     # visit NASA Mars news site
     news_url = "https://mars.nasa.gov/news/"
@@ -29,17 +26,12 @@ def scrape_news():
 
 
     # Scrape the NASA Mars News Site and collect the latest News Title and Paragraph Text.
-    try:
-        news_title = news_soup.find_all('div', class_="content_title")[1].text
-        news_p = news_soup.find_all('div', class_="article_teaser_body")[0].text
+    news_title = news_soup.find_all('div', class_="content_title")[1].text
+    news_p = news_soup.find_all('div', class_="article_teaser_body")[0].text
 
-        # Add entries into dictionary
-        mars_info['news_title'] = news_title
-        mars_info['news_p'] = news_p
-
-    # except:   
-        # return None
-    # return news_title, news_p
+    # Add entries into dictionary
+    mars_info['news_title'] = news_title
+    mars_info['news_p'] = news_p
 
 
 # JPL Mars Space Images
@@ -64,6 +56,7 @@ def scrape_image():
     # Add entry into dictionary
     mars_info['featured_image_url'] = featured_image_url
 
+
 # Mars Facts
 def scrape_facts():
 
@@ -73,6 +66,66 @@ def scrape_facts():
     # Use Panda's `read_html` to parse the url
     tables = pd.read_html(facts_url)
 
+    # create dataframe
+    facts_df = tables[0]
+    facts_df.columns = ['Description', 'Value']
+    facts_df.set_index('Description', inplace=True)
+
+    
+    # Use Pandas to convert the data to a HTML table string.
+    html_table = facts_df.to_html()
+
+    # Add entry into dictionary
+    mars_info['html_table'] = html_table
 
 
+# Mars Hemispheres
+def scrape_hemispheres():
 
+    # Read HTML from website and create a Beautiful Soup object
+    hemi_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    hemi_base_url = "https://astrogeology.usgs.gov"
+    browser.visit(hemi_url)
+
+    # Scrape page into Soup
+    hemi_html =browser.html
+    hemi_soup = bs(hemi_html, 'html.parser')
+
+    # extract instances of hemisphere information
+    hemispheres = hemi_soup.find_all('div', class_="item")
+    # hemispheres
+
+    #create empty list to store urls 
+    hemi_image_urls = []
+
+    # Loop through hemisphere data
+    for x in hemispheres:
+        
+        # extract enhanced image url and store in empty list
+        hemi_image_urls.append(f"{hemi_base_url}{x.find('a', class_='itemLink')['href']}")
+
+    #create empty list to store urls 
+    hemisphere_urls = []
+
+    for link in hemi_image_urls:
+        browser.visit(link)
+        
+        time.sleep(1)
+        
+        hemi_html =browser.html
+        hemi_soup = bs(hemi_html, 'html.parser')
+
+        image_url = hemi_soup.find('img', class_="wide-image")['src']
+        title = hemi_soup.find('h2', class_="title").text
+
+        # create Python dictionary to store data
+        hemisphere_dict = {
+            'title':title,
+            'img_url': hemi_base_url + image_url
+        }
+
+        hemisphere_urls.append(hemisphere_dict)
+
+    mars_info['hemisphere_urls'] = hemisphere_urls
+
+    return mars_info
